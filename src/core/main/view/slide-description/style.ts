e97685e7c2bf5371e0e1472a8bc9ba3a5f850b9d
“Salar <salar.sali97@gmail.com>
2024-03-16 00:43:43 +0300

    slide desctiption width refactored

--- src/core/main/view/slide-description/style.ts
+++ src/core/main/view/slide-description/style.ts
@@ -2,13 +2,25 @@ import styled from "styled-components";
 import * as pallete from "../../../../bootstrap/helper/global-helper";
 
 export const MainPageSlideDescription = styled.div`
-  width: 45%;
   position: absolute;
   top: 50%;
-  font-size: 36px;
   color: white;
   right: 5%;
   transform: translateY(-50%);
+  width: 45%;
+  font-size: 36px;
+
+  @media (max-width: 1024px) {
+    width: 60%;
+  }
+
+  @media (max-width: 768px) {
+    width: 80%;
+  }
+
+  @media (max-width: 480px) {
+    width: 90%;
+  }
 `;
 
 export const MainPageSlideDescriptionTitle = styled.span`
