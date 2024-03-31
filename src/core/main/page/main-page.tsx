81ece25acd814af1f81b6da95c289968d00ea513
“Salar <salar.sali97@gmail.com>
2024-03-31 19:28:01 +0300

    repeated questions main logic

--- src/core/main/page/main-page.tsx
+++ src/core/main/page/main-page.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import MainHotelCardsWrapper from "~/core/main/view/hotel-cards-wrapper/main-hotel-cards-wrapper";
 import MainOurServicesWrapper from "~/core/main/view/our-services-section/wrapper/main-our-services-wrapper";
+import MainRepeatedQuestionsWrapper from "~/core/main/view/repeated-questions-section/wrapper/main-repeated-questions-wrapper";
 import MainSlides from "~/core/main/view/slides/main-slides";
 
 // TODO delete this
@@ -18,6 +19,7 @@ const MainPage = () => {
       <MainSlides />
       <MainHotelCardsWrapper />
       <MainOurServicesWrapper />
+      <MainRepeatedQuestionsWrapper />
       <StyledDiv>salar</StyledDiv>
     </div>
   );
