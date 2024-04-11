d3222b2601ea65f86b8581e4f0aa0e39ad0ea4cc
“Salar <salar.sali97@gmail.com>
2024-04-12 02:01:24 +0300

    wrapper style fixed

--- src/core/chalets/view/cards-section/wrapper/chalets-cards-wrapper.tsx
+++ src/core/chalets/view/cards-section/wrapper/chalets-cards-wrapper.tsx
@@ -1,30 +1,15 @@
-import styled from "styled-components";
-import { HandlingSectionPaddingWrapper } from "~/bootstrap/helper/global-styles";
 import ChaletsCardsCard from "~/core/chalets/view/cards-section/card/chalets-cards-card";
-import * as palette from "~/bootstrap/helper/global-helper";
-
-const StyledHandlingSectionPaddingWrapper = styled(
-  HandlingSectionPaddingWrapper
-)`
-  display: flex;
-  justify-content: start;
-  flex-wrap: wrap;
-  gap: 16px;
-
-  @media (max-width: ${palette.smallScreenSize}) {
-    justify-content: space-between;
-  }
-`;
+import { StyledChaletsCardsWrapper } from "~/core/chalets/view/cards-section/wrapper/style";
 
 const ChaletsCardsWrapper = () => {
   return (
-    <StyledHandlingSectionPaddingWrapper>
+    <StyledChaletsCardsWrapper>
       <ChaletsCardsCard />
       <ChaletsCardsCard />
       <ChaletsCardsCard />
       <ChaletsCardsCard />
       <ChaletsCardsCard />
-    </StyledHandlingSectionPaddingWrapper>
+    </StyledChaletsCardsWrapper>
   );
 };
 
---
+++ src/core/chalets/view/cards-section/wrapper/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import * as palette from "~/bootstrap/helper/global-helper";
+import { HandlingSectionPaddingWrapper } from "~/bootstrap/helper/global-styles";
+
+export const StyledChaletsCardsWrapper = styled(HandlingSectionPaddingWrapper)`
+  display: flex;
+  justify-content: start;
+  flex-wrap: wrap;
+  gap: 16px;
+
+  @media (max-width: ${palette.smallScreenSize}) {
+    justify-content: space-between;
+  }
+`;
