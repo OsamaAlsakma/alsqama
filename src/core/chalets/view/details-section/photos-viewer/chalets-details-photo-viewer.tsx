5ed06c286b2e72cc242768a49d8436844fc577fa
“Salar <salar.sali97@gmail.com>
2024-05-01 19:52:53 +0300

    images implmented in the details page

--- src/core/chalets/view/details-section/photos-viewer/chalets-details-photo-viewer.tsx
+++ src/core/chalets/view/details-section/photos-viewer/chalets-details-photo-viewer.tsx
@@ -3,6 +3,17 @@ import { useCallback, useState } from "react";
 import ImageViewer from "react-simple-image-viewer";
 import { styled } from "styled-components";
 import { HandlingSectionPaddingWrapper } from "~/bootstrap/helper/global-styles";
+import * as palette from "~/bootstrap/helper/global-helper";
+import {
+  StyledSwiper,
+  StyledSwiperSlide,
+  CustomPrevButton,
+  CustomNextButton,
+} from "~/core/main/view/slides/style";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
 
 const images = [
   "https://c4.wallpaperflare.com/wallpaper/915/596/21/dicksam-plateau-socotra-island-yemen-dragon-trees-desert-landscape-desktop-wallpaper-hd-1920%C3%971080-wallpaper-preview.jpg",
@@ -13,6 +24,7 @@ const images = [
   "https://c4.wallpaperflare.com/wallpaper/783/466/557/nature-trees-rock-dragon-blood-tree-wallpaper-preview.jpg",
 ];
 
+/* ------------------------- First position styling ------------------------- */
 const ChaletsDetailsPhotoAllImagesWrapper = styled.div`
   width: 100%;
   border-radius: 16px;
@@ -20,6 +32,9 @@ const ChaletsDetailsPhotoAllImagesWrapper = styled.div`
   display: flex;
   gap: 8px;
   height: 500px;
+  @media (max-width: ${palette.largeScreenSize}) {
+    display: none;
+  }
 `;
 
 const ChaletsDetailsPhotoMainImage = styled(CardMedia)`
@@ -42,6 +57,20 @@ const ChaletsDetailsPhotoSmallImage = styled(CardMedia)`
   object-fit: cover;
 `;
 
+/* ------------------------- Second position styling ------------------------ */
+const TestSwiper = styled(StyledSwiper)`
+  border-radius: 16px;
+  overflow: hidden;
+  display: none;
+  height: 500px;
+  @media (max-width: ${palette.largeScreenSize}) {
+    display: block;
+  }
+  @media (max-width: ${palette.smallScreenSize}) {
+    height: 350px;
+  }
+`;
+/* ----------------------------- Implementation ----------------------------- */
 const ChaletsDetailsPhotoViewer = () => {
   const [currentImage, setCurrentImage] = useState(0);
   const [isViewerOpen, setIsViewerOpen] = useState(false);
@@ -76,8 +105,32 @@ const ChaletsDetailsPhotoViewer = () => {
           ))}
         </ChaletsDetailsPhotoSmallImagesWrapper>
       </ChaletsDetailsPhotoAllImagesWrapper>
-      {/* second position */}
 
+      {/* second position */}
+      <TestSwiper
+        navigation={{
+          prevEl: ".swiper-button-prev",
+          nextEl: ".swiper-button-next",
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        loop={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {images.map((image, index) => {
+          return (
+            <StyledSwiperSlide
+              key={index}
+              style={{ backgroundImage: `url(${image})` }}
+              onClick={() => openImageViewer(index)}
+            />
+          );
+        })}
+        <CustomPrevButton className="swiper-button-prev" />
+        <CustomNextButton className="swiper-button-next" />
+      </TestSwiper>
       {/* open button */}
       <Button onClick={() => setIsViewerOpen(true)}>عرض المزيد من الصور</Button>
       {isViewerOpen && (
