053626c387af570780c051d5c9fa19248e9588a8
“Salar <salar.sali97@gmail.com>
2024-05-08 20:22:49 +0300

    logo added

--- src/generic/components/main-logo/main-logo.tsx
+++ src/generic/components/main-logo/main-logo.tsx
@@ -1,8 +1,15 @@
-import { servicesPageEndpoint } from "../../../bootstrap/helper/endpoints";
-import { StyledMainLogo } from "./style";
+import { servicesPageEndpoint } from "~/bootstrap/helper/endpoints";
+import {
+  StyledMainLogo,
+  StyledMainLogoImage,
+} from "~/generic/components/main-logo/style";
 
 const MainLogo = () => {
-  return <StyledMainLogo to={servicesPageEndpoint.main}>حجزي</StyledMainLogo>;
+  return (
+    <StyledMainLogo to={servicesPageEndpoint.main}>
+      <StyledMainLogoImage src="/assets/main-logo.png" />
+    </StyledMainLogo>
+  );
 };
 
 export default MainLogo;
--- src/generic/components/main-logo/style.ts
+++ src/generic/components/main-logo/style.ts
@@ -2,6 +2,14 @@ import styled from "styled-components";
 import { StyledMainServicesLink } from "../main-services/style";
 
 export const StyledMainLogo = styled(StyledMainServicesLink)`
-  font-size: 32px;
-  width: fit-content;
+  display: flex;
+  align-items: center;
+  background-color: red;
+  border-radius: 16px;
+  overflow: hidden;
+`;
+
+export const StyledMainLogoImage = styled.img`
+  width: 100px;
+  height: 50px;
 `;
