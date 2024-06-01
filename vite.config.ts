49ccc7f28862b623c77c27ab632039e13fea927d
“Salar <salar.sali97@gmail.com>
2024-06-01 23:58:03 +0300

    visualizer added

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import {  visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,12 @@ export default defineConfig({
         babelrc: true,
       },
     }),
+    visualizer({
+      template: "treemap",
+      gzipSize: true,
+      brotliSize: true,
+      filename: "analyse.html",
+    }) 
   ],
 
   esbuild: {
