85d940af472b8fd244b1f60fc6696ad591aec1ff
“Salar <salar.sali97@gmail.com>
2024-06-09 01:05:10 +0300

    filteration based on start and end dates added

--- src/core/hotels/view/items-cards-section/filteration/start-and-end-dates/hotels-hotel-items-filteration-start-and-end-dates.tsx
+++ src/core/hotels/view/items-cards-section/filteration/start-and-end-dates/hotels-hotel-items-filteration-start-and-end-dates.tsx
@@ -4,8 +4,11 @@ import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import dayjs, { Dayjs } from "dayjs";
 import { SetStateAction, useState } from "react";
 import styled from "styled-components";
-import { secondaryColor } from "~/bootstrap/helper/global-helper";
+import { primaryColor, secondaryColor } from "~/bootstrap/helper/global-helper";
 
+/* -------------------------------------------------------------------------- */
+/*                                   Styling                                  */
+/* -------------------------------------------------------------------------- */
 const DetailsBookingCardInputsSection = styled.div`
   display: flex;
   gap: 8px;
@@ -25,14 +28,12 @@ const StyledDatePicker = styled(DatePicker)`
     && .MuiOutlinedInput-notchedOutline {
       border: none;
     }
-
-    & .Mui-selected-root {
-      background-color: #f95d44 !important;
-      color: "white";
-    }
   }
 `;
 
+/* -------------------------------------------------------------------------- */
+/*                                  Component                                 */
+/* -------------------------------------------------------------------------- */
 const HotelsHotelItemsFilterationStartAndEndDates = () => {
   const today = dayjs(new Date());
   const tomorrow = dayjs().add(1, "day");
@@ -53,6 +54,16 @@ const HotelsHotelItemsFilterationStartAndEndDates = () => {
             onChange={(newValue: SetStateAction<dayjs.Dayjs | null>) =>
               setStartDate(newValue)
             }
+            slotProps={{
+              day: {
+                sx: {
+                  "&.MuiPickersDay-root.Mui-selected": {
+                    backgroundColor: `${primaryColor}`,
+                    color: "white",
+                  },
+                },
+              },
+            }}
           />
         </DetailsBookingCardInput>
         <DetailsBookingCardInput className="to-input">
